887a166bbe911f87878366ff62f10ebed76a3357
Jason Brain <19595939+jbrain4@users.noreply.github.com>
2018-08-18 17:23:44 -0600

    Added getters and setters for min, max & word count. Added some comments

--- main.py
+++ main.py
@@ -6,21 +6,55 @@ class PasswordGenerator:
         self.dictionary = open("dict.txt")
 
         self.numbers = "0123456789"
+
+        # define the symbols to use as separators
         self.symbols = ",.\\/?*&%#@!:;~-_=+"
+
+        self.numberOfWords = 0
+        self.minimumWordLength = 0
+        self.maximumWordLength = 0
 
         # Save the dictionary as an array
         self.words = self.dictionary.readlines()
         self.words[:] = [self.word.rstrip('\n') for self.word in self.words]
         ## DEBUG: print(words)
 
-    def generatePassword(self, wordTotal, min, max):
+
+    # GETTERS AND SETTERS BELOW ################################################
+    # Set the number of words to use in the password
+    def setNumberOfWords(self, number):
+        self.numberOfWords = number
+
+    # Get the number of words to use in the password
+    def getNumberOfWords(self):
+        return self.numberOfWords
+
+    # Set the minimum word length
+    def setMinimumWordLength(self, number):
+        self.minimumWordLength = number
+
+    # Get the minimum word length
+    def getMinimumWordLength(self):
+        return self.minimumWordLength
+
+    # Set the maximum word length
+    def setMaximumWordLength(self, number):
+        self.maximumWordLength = number
+
+    # Get the maximum word length
+    def getMaximumWordLength(self):
+        return self.maximumWordLength
+
+
+
+    def generatePassword(self):
         password = ""
         currentWord = 0
         caseStart = int(random.choice(self.numbers))
         separator = random.choice(self.symbols)
-        while currentWord < wordTotal:
+        while currentWord < self.numberOfWords:
             newWord = random.choice(self.words)
-            if len(newWord) < min or len(newWord) > max:
+            if len(newWord) < self.minimumWordLength or len(newWord) > self.maximumWordLength:
                 continue
             else:
                 if (caseStart % 2) == 0:
@@ -29,18 +63,24 @@ class PasswordGenerator:
                 else:
                     password += newWord
                     caseStart += 1
-            if (currentWord + 1) < wordTotal:
+            if (currentWord + 1) < self.numberOfWords:
                 password += separator
             currentWord += 1
         password += random.choice(self.numbers)
         password += random.choice(self.numbers)
         return password
+
+    # Close the dictionary file
     def cleanup(self):
         self.dictionary.close()
 
+## DEBUG:
 passwd = PasswordGenerator()
-numberOfWords = 3
-wordLengthMinimum = 4
-wordLengthMaximum = 10
-print(passwd.generatePassword(numberOfWords, wordLengthMinimum, wordLengthMaximum))
+passwd.setNumberOfWords(3)
+passwd.setMinimumWordLength(4)
+passwd.setMaximumWordLength(10)
+print(passwd.getNumberOfWords())
+print(passwd.getMaximumWordLength())
+print(passwd.getMinimumWordLength())
+print(passwd.generatePassword())
 passwd.cleanup()
