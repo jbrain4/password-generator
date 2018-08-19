c5d51227aec524f5c4af622314098fcd12c76449
Jason Brain <19595939+jbrain4@users.noreply.github.com>
2018-08-19 10:57:40 -0600

    Added get/set for pre/post digit padding.

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ class PasswordGenerator:
         # Open the dictionary
         self.dictionary = open("dict.txt")
 
-        self.numbers = "0123456789"
-
         # define the symbols to use as separators
         self.symbols = ",.\\/?*&%#@!:;~-_=+"
 
         self.numberOfWords = 0
         self.minimumWordLength = 0
         self.maximumWordLength = 0
+        self.paddingDigitsBefore = 0
+        self.paddingDigitsAfter = 0
 
         # Save the dictionary as an array
         self.words = self.dictionary.readlines()
@@ -20,7 +20,7 @@ class PasswordGenerator:
         ## DEBUG: print(words)
 
 
-    # GETTERS AND SETTERS BELOW ################################################
+# GETTERS AND SETTERS BELOW ####################################################
     # Set the number of words to use in the password
     def setNumberOfWords(self, number):
         self.numberOfWords = number
@@ -45,13 +45,39 @@ class PasswordGenerator:
     def getMaximumWordLength(self):
         return self.maximumWordLength
 
+    # Set the number of padding digits at the start of the password
+    def setPaddingDigitsBefore(self, number):
+        self.paddingDigitsBefore = number
+
+    # Get the number of padding digits at the start of the password
+    def getPaddingDigitsBefore(self):
+        return self.paddingDigitsBefore
+
+    # Set the number of padding digits at the end of the password
+    def setPaddingDigitsAfter(self, number):
+        self.paddingDigitsAfter = number
+
+    # Get the number of padding digits at the end of the password
+    def getPaddingDigitsAfter(self):
+        return self.paddingDigitsAfter
 
 
+# PASSWORD GENERATOR ###########################################################
     def generatePassword(self):
         password = ""
         currentWord = 0
-        caseStart = int(random.choice(self.numbers))
+        startDigitPaddingIndex = 0
+        endDigitPaddingIndex = 0
+        caseStart = random.randint(0, 9)
         separator = random.choice(self.symbols)
+
+        while startDigitPaddingIndex < self.paddingDigitsBefore:
+            password += str(random.randint(0, 9))
+            startDigitPaddingIndex += 1
+
+        if self.paddingDigitsBefore > 0:
+            password += separator
+
         while currentWord < self.numberOfWords:
             newWord = random.choice(self.words)
             if len(newWord) < self.minimumWordLength or len(newWord) > self.maximumWordLength:
@@ -66,8 +92,14 @@ class PasswordGenerator:
             if (currentWord + 1) < self.numberOfWords:
                 password += separator
             currentWord += 1
-        password += random.choice(self.numbers)
-        password += random.choice(self.numbers)
+
+        if self.paddingDigitsAfter > 0:
+            password += separator
+
+        while endDigitPaddingIndex < self.paddingDigitsAfter:
+            password += str(random.randint(0, 9))
+            endDigitPaddingIndex += 1
+
         return password
 
     # Close the dictionary file
@@ -79,8 +111,7 @@ passwd = PasswordGenerator()
 passwd.setNumberOfWords(3)
 passwd.setMinimumWordLength(4)
 passwd.setMaximumWordLength(10)
-print(passwd.getNumberOfWords())
-print(passwd.getMaximumWordLength())
-print(passwd.getMinimumWordLength())
+passwd.setPaddingDigitsBefore(2)
+passwd.setPaddingDigitsAfter(2)
 print(passwd.generatePassword())
 passwd.cleanup()
